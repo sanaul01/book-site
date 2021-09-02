2fb2442992be427e5e215030e1d9d5e8a896b215
sanaul01 <mdsanaulislam154@gmail.com>
2021-09-02 13:21:56 +0600

    second commit added

--- js/book-archive.js
+++ js/book-archive.js
@@ -1,6 +1,7 @@
 const loadData = () =>{
     const searchInput = document.getElementById('input-text');
     const searchText = searchInput.value;
+    // searchInput.value = '';
     console.log(searchText)
 
     const url = `http://openlibrary.org/search.json?q=${searchText}`
@@ -10,19 +11,21 @@ const loadData = () =>{
 }
 
 const displayBook = books =>{
+    document.getElementById('books-numbers').innerText = `Find Books: ${books.length}`; 
     const bookField = document.getElementById('book-field')
+    bookField.textContent = '';
     books.forEach(book =>{
         console.log(book)
         const div = document.createElement('div');
         div.classList.add('col')
         div.innerHTML = `
             <div class="card h-100">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">${book.title}</h5>
+                    <h5 class="card-title"><span class="text-primary">Book Name:</span> ${book.title}</h5>
                     <h5 class="card-title text-danger"><span class="text-primary">Author Name:</span> ${book.author_name}</h5>
-                    <h5 class="card-title">${book.first_publish_year}</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 class="card-title"><span class="text-primary">First Publish:</span> ${book.first_publish_year}</h5>
+                    <p class="card-text">Text: ${book.text.slice(0, 10)}</p>
                 </div>
             </div>
         `;
